7147175846b2d59ba3d4ed60db3d500b93541f56
Egor Samoylov <mr.egoras@mail.ru>
2025-02-07 17:33:10 +0300

    Внутри компонента BookList создание асинхронного метода fetchBooks, который вытаскивает книги с сервера. Монтирование этого метода. Задание переменной loading для указания на загрузку старницы (использование условного рендеринга для основного div).

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,7 +5,8 @@
       <input type="text" v-model="searchTitle" placeholder="Поиск по названию">
       <input type="text" v-model="searchAuthor" placeholder="Поиск по автору">
     </div>
-    <div class="book-List">
+    <div v-if="loading"></div>
+    <div v-else class="book-List">
       <div v-for="book in filteredBooks" :key="book.id">
         <div>
           <div class="book-content">
@@ -13,7 +14,7 @@
             <p>
               Автор: {{ book.author }}. <br/>
               Год: {{ book.publishedYear }} <br/>
-              Статус: {{ book.isBorrowed }}
+              Статус: {{ book.borrowed }}
             </p>
           </div>
           <div class="book-buttons">
@@ -26,27 +27,46 @@
 </template>
 
 <script>
+import axios from '@/plugins/axios';
+
   export default {
     data() {
       return {
         books: [
-          {id: 1, title: "Book title 1", author: "Book author 1", publishedYear: 1997, isBorrowed: true},
-          {id: 2, title: "Book title 2", author: "Book author 2", publishedYear: 1999, isBorrowed: false},
-          {id: 3, title: "Book title 3", author: "Book author 3", publishedYear: 1990, isBorrowed: true},
+          {id: 1, title: "Book title 1", author: "Book author 1", publishedYear: 1997, borrowed: true},
+          {id: 2, title: "Book title 2", author: "Book author 2", publishedYear: 1999, borrowed: false},
+          {id: 3, title: "Book title 3", author: "Book author 3", publishedYear: 1990, borrowed: true},
         ],
         searchTitle: '',
-        searchAuthor: ''
+        searchAuthor: '',
+        loading: false
       };
     },
     computed: {
-    filteredBooks() {
-      return this.books.filter(book => {
-        const titleMatch = book.title.toLowerCase().includes(this.searchTitle.toLowerCase());
-        const authorMatch = book.author.toLowerCase().includes(this.searchAuthor.toLowerCase());
-        return titleMatch && authorMatch;
-      });
-    }
-  },
+      filteredBooks() {
+        return this.books.filter(book => {
+          const titleMatch = book.title.toLowerCase().includes(this.searchTitle.toLowerCase());
+          const authorMatch = book.author.toLowerCase().includes(this.searchAuthor.toLowerCase());
+          return titleMatch && authorMatch;
+        });
+      }
+    },
+    methods: {
+      async fetchBooks() {
+        this.loading = true;
+        try {
+          const response = await axios.get('/books');
+          this.books = response.data;
+        } catch (error) {
+          console.error('Ошибка при загрузке книг:', error);
+        } finally {
+          this.loading = false;
+        }
+      },
+    },
+    mounted() {
+      this.fetchBooks();
+    },
   }
 </script>
 
