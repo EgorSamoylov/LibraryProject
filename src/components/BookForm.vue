45d4d2bf075e9e468043e8702a675586a438515b
Egor Samoylov <mr.egoras@mail.ru>
2025-02-07 22:02:50 +0300

    Внутри BookForm дополнили input и button для отмены действия. Написали метод, который обрабатывает отправку данных формы на сервер для создания или обновления книги. Также написали вотчер, который обеспечивает однонаправленную привязку данных

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -4,19 +4,21 @@
         <!-- @submit.prevent="handleSubmit" - предотвращает обновления страницы -->
         <form @submit.prevent="handleSubmit">
             <div>
-                <input placeholder="название книги"/>
-                <input placeholder="Автор"/>
-                <input placeholder="год издания"/>
+                <input placeholder="название книги" id="title" v-model="book.title" required/>
+                <input placeholder="Автор" id="author" v-model="book.author" required/>
+                <input placeholder="год издания" id="publishedYear" v-model.number="book.publishedYear" required/>
             </div>
             <div>
                 <button type="submit">{{ isEditMode ? 'Сохранить' : 'Добавить' }}</button>
-                <button type="button">Отмена</button>
+                <button type="button" @click="cancel">Отмена</button>
             </div>
         </form>
     </div>
 </template>
 
 <script>
+import axios from '@/plugins/axios';
+
 export default {
     data() {
         return {
@@ -26,6 +28,31 @@ export default {
     computed: {
         isEditMode() {
           return this.book.id !== undefined;
+        }
+    },
+    watch: {
+        initialBook: {
+          handler(newVal) {
+            this.book = { ...newVal };
+          },
+          deep: true
+        }
+    },
+    methods: {
+        async handleSubmit() {
+            try {
+                if (this.isEditMode) {
+                  await axios.put(`/books/${this.book.id}`, this.book);
+                } else {
+                  await axios.post('/books', this.book);
+                }
+                this.$emit('book-submitted');
+            } catch (error) {
+                console.error('Ошибка при сохранении книги:', error);
+            }
+        },
+        cancel() {
+            this.$emit('cancel');
         }
     }
 }
