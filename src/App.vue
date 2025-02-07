7248a458ad209249cb3e88d54425da487d2b883e
Egor Samoylov <mr.egoras@mail.ru>
2025-02-07 20:29:13 +0300

    Написание стилей для BookList.vue (для списка, инпутов, кнопок и самих единиц книг) и App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,24 +2,24 @@
   <div>
     <h2>Список книг</h2>
     <div>
-      <input type="text" v-model="searchTitle" placeholder="Поиск по названию">
-      <input type="text" v-model="searchAuthor" placeholder="Поиск по автору">
+      <input class="input" type="text" v-model="searchTitle" placeholder="Поиск по названию">
+      <input class="input" type="text" v-model="searchAuthor" placeholder="Поиск по автору">
     </div>
     <div v-if="loading"></div>
-    <div v-else class="book-List">
-      <div v-for="book in filteredBooks" :key="book.id">
-        <div>
+    <div v-else>
+      <div class="book-List" v-for="book in filteredBooks" :key="book.id">
+        <div class="book-item">
           <div class="book-content">
-            <h3>{{ book.id }}. {{ book.title }}</h3>
-            <p>
+            <strong>{{ book.id }}. {{ book.title }}</strong>
+            <div class="book-p">
               Автор: {{ book.author }}. <br/>
               Год: {{ book.publishedYear }} <br/>
               Статус: {{ book.borrowed }}
-            </p>
+            </div>
           </div>
           <div class="book-buttons">
-            <button @click="deleteBook(book.id)">Удалить</button>
-            <button @click="toggleBorrowStatus(book)">{{ book.borrowed ? 'Вернуть' : 'Взять' }}</button>
+            <button class="btn" @click="deleteBook(book.id)">Удалить</button>
+            <button class="btn" @click="toggleBorrowStatus(book)">{{ book.borrowed ? 'Вернуть' : 'Взять' }}</button>
           </div>
         </div>
       </div>
@@ -90,4 +90,76 @@ import axios from '@/plugins/axios';
 
 <style>
 
+.book-List {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+}
+
+.book-item {
+    display: flex;
+    padding: 15px;
+    border: 0.8px solid rgb(220, 227, 235);
+    border-radius: 24px;
+    margin-top: 15px;
+    justify-content: space-between;
+    font-family: Arial, Helvetica, sans-serif;
+    width: 50%;
+}
+
+.book-item:hover {
+  border: 0.8px solid rgb(42, 49, 55);
+}
+
+.book-content {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+
+.book-p {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.book-buttons {
+  display: flex;
+  flex-direction: column;
+}
+
+/* Стили на UI компонентов */
+.btn {
+    padding: 5px 15px;
+    color: white;
+    background-color: teal;
+    border: solid 2px teal;
+    text-decoration-color:teal;
+    transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+    font-family: Arial, Helvetica, sans-serif;
+    border-radius: 8px;
+    margin: 5px 10px;
+    height: 30px;
+    cursor: pointer;
+}
+
+.btn:hover {
+    background-color: rgb(0, 155, 155);
+    border: solid 2px gb(0, 155, 155);
+    color: white;
+}
+
+.input {
+    width: 50%;
+    padding: 5px 15px;
+    margin: 5px 0px;
+    border: 1px solid rgb(42, 49, 55);
+    border-radius: 8px;
+    box-shadow: rgb(220, 227, 235) 0px 0px 0px 1px;
+    font-family: Arial, Helvetica, sans-serif;
+    height: 20px;
+}
 </style>
